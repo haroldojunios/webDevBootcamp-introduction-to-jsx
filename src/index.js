5a22443e9bcec9b040c6d9f53180e7391e34507a
Haroldo Júnio <haroldojunios@gmail.com>
2021-12-16 08:37:12 -0300

    Put images instead of food names

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import ReactDOM from "react-dom";
 ReactDOM.render(
   <div>
     <h1 className="heading" contentEditable="true" spellCheck="false">My favorite foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <div>
+      <img className="food-img" src="https://www.receitasnestle.com.br/sites/default/files/2020-12/capa-bacon-o-que-e-como-fazer-e-melhores-receitas.jpg" alt="" />
+      <img className="food-img" src="https://ogimg.infoglobo.com.br/in/25038759-fec-56d/FT631A/pata-negra.jpg" alt="" />
+      <img className="food-img" src="https://conteudo.imguol.com.br/2014/07/16/passo-a-passo-noodles-de-pequim-1405524977562_1024x768.jpg" alt="" />
+    </div>
   </div>,
   document.getElementById("root")
 );
